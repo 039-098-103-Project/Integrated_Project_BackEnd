95afb401dba8946142b7fd7f9c120b6d65d702a0
zyct <thanyaluck.yict@mail.kmutt.ac.th>
2021-05-01 23:44:04 +0700

    [UPDATE] Product Not Found Exceotion

--- src/main/java/int221/project/controllers/ProductController.java
+++ src/main/java/int221/project/controllers/ProductController.java
@@ -1,9 +1,12 @@
 package int221.project.controllers;
 
+import int221.project.exceptions.ProductNotFoundException;
 import int221.project.models.Product;
 import int221.project.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -15,29 +18,41 @@ public class ProductController {
     ProductService productService;
 
     @GetMapping("/product")
-    public List<Product> showAll(){
+    public List<Product> showAll() {
         return productService.getAllProducts();
     }
 
     @GetMapping("/product/{id}")
-    public Optional<Product> show(@PathVariable int id){
-        return productService.getProduct(id);
+    public Optional<Product> show(@PathVariable int id) {
+        try {
+            return productService.getProduct(id);
+        } catch (ProductNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product Not Found.", e);
+        }
     }
 
     @PutMapping("/product/edit/{id}")
-    public Optional<Product> edit(@RequestBody Product newProduct, @PathVariable int id){
-        return productService.editProduct(newProduct,id);
+    public Optional<Product> edit(@RequestBody Product newProduct, @PathVariable int id) {
+        try {
+            return productService.editProduct(newProduct, id);
+        } catch (ProductNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product Not Found.", e);
+        }
     }
 
     @PostMapping("/addProduct")
-    public void add(@RequestBody Product newProduct){
+    public void add(@RequestBody Product newProduct) {
         productService.addProduct(newProduct);
     }
 
     @Transactional
     @DeleteMapping("/product/delete/{id}")
-    public void delete(@PathVariable int id){
-        productService.deleteProduct(id);
+    public void delete(@PathVariable int id) {
+        try {
+            productService.deleteProduct(id);
+        } catch (ProductNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product Not Found.", e);
+        }
     }
 
 
