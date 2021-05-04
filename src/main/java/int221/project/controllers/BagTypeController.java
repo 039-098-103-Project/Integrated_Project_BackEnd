a3c1afaab94cfba5fc840110eefd237e246edb2a
zyct <thanyaluck.yict@mail.kmutt.ac.th>
2021-05-04 22:17:25 +0700

    [UPDATE] product images, GET Method Request for Color, BagType

---
+++ src/main/java/int221/project/controllers/BagTypeController.java
@@ -0,0 +1,20 @@
+package int221.project.controllers;
+
+import int221.project.models.BagType;
+import int221.project.services.BagTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class BagTypeController {
+    @Autowired
+    BagTypeService bagTypeService;
+
+    @GetMapping("/bagtypes")
+    public List<BagType> getAll(){
+        return bagTypeService.getAllBagType();
+    }
+}
